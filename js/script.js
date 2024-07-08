ac5e6049810b443fc16b809212ee0c5970b9096e
SimoneDUrso <simone.durso98@gmail.com>
2024-07-08 19:52:47 +0200

    [UPD] Added variables, function and cycles. Exercise completed.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,57 @@ function createSquare() {
 
     // Restituisco il quadrato contenuto nella variabile sopra
     return currentElement;
+}
 
+// Funzione per il controllo dei quadrati e l'assegnazione della classe clicked-bomb o per l'incremento dello score
+function checkSquares() {
+    // console.log(this.innerText);
+
+    if (gameOver) {
+        return
+    }
+    let squareNumber = parseInt(this.innerText);
+    if (bombList.includes(squareNumber)) {
+        this.classList.add("clicked-bomb");
+        this.removeEventListener("click", checkSquares);
+
+        alert(`Hai perso con un punteggio di ${score}`)
+        gameOver = true;
+    }
+
+    else {
+        this.classList.add("clicked-azure");
+        score++;
+    }
+
+    this.removeEventListener("click", checkSquares);
+    document.getElementById("score").innerText = score;
+
+    if (score === totalCells - totalBombs) {
+        alert("Che fortuna...!");
+        gameOver = true;
+    }
+}
+
+// Dichiarazione variabili
+let bombList = [];
+const totalCells = 100;
+const totalBombs = 16;
+let score = 0;
+let gameOver = false;
+
+// Funzione per la generazione delle bombe che richiameremo al click del bottone
+function bombsGenerate() {
+
+    bombList = [];
+
+    // WHILE per la creazione randomica dei delle bombe in determinati quadratini
+    while (bombList.length < totalBombs) {
+        let number = Math.floor(Math.random() * totalCells + 1);
+        if (!bombList.includes(number)) {
+            bombList.push(number);
+        }
+    }
 }
 
 // Recupero il pulsante presente nell'header
@@ -21,6 +71,12 @@ const grid = document.getElementById("grid")
 btn.addEventListener("click", function () {
 
     grid.innerHTML = "";
+    score = 0;
+    gameOver = false;
+
+    bombsGenerate();
+
+    console.log(bombList);
     // Ciclo FOR per creare una griglia 10x10
     for (i = 0; i < 100; i++) {
 
@@ -28,10 +84,7 @@ btn.addEventListener("click", function () {
         let currentSquare = createSquare();
 
         // Evento click sul singolo quadrato
-        currentSquare.addEventListener("click", function () {
-            this.classList.add("clicked-azure")
-            console.log(currentSquare.innerText);
-        });
+        currentSquare.addEventListener("click", checkSquares)
 
 
         // Inseriamo un numero progressivo
